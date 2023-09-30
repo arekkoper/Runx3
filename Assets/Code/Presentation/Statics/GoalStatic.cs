18ee3547aa19ec1f182c7041e12fb8e2237be2d7
Arkadiusz Koper <arkadiusz.koper1990@gmail.com>
2023-09-30 10:18:08 +0200

    add static end

---
+++ Assets/Code/Presentation/Statics/GoalStatic.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Code.Application.Commons.Interfaces.Mediator;
+using Assets.Code.Application.Modules.Game.Commands.LoadLevel;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Code.Presentation.Presenters
+{
+    public class GoalStatic : MonoBehaviour
+    {
+        [Inject] private readonly SignalBus _signalBus;
+
+        private void Awake()
+        {
+            DiContainer container = ProjectContext.Instance.Container;
+
+            container.Inject(this);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            _signalBus.Fire(other);
+        }
+    }
+}
