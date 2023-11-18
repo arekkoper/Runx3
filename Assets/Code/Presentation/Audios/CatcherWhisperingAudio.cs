51ec4f7f6ca1685753e24f81d275e0241aedbe42
Arkadiusz Koper <arkadiusz.koper1990@gmail.com>
2023-11-18 18:55:58 +0100

    add catcher whispering

---
+++ Assets/Code/Presentation/Audios/CatcherWhisperingAudio.cs
@@ -0,0 +1,28 @@
+﻿using Code.Application.Commons.Interfaces.Services;
+using Code.Application.Commons.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+using AudioSettings = Code.Application.Commons.Structs.AudioSettings;
+
+namespace Assets.Code.Presentation.Audios
+{
+    public class CatcherWhisperingAudio : MonoBehaviour
+    {
+        [SerializeField] private AudioSettings settings;
+
+        [Inject] private readonly IAudioService _audioService;
+
+        private void Start()
+        {
+            _audioService.ChangeAudioSourcePosition(transform);
+            _audioService.Configure($"Audio (Whispering)", settings);
+
+            _audioService.Play();
+        }
+    }
+}
